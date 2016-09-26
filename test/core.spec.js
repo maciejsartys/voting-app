0daab549ed35f4a80a2bb86b27586faf687ed87e
Maciej Sartys <sartys.maciej@gmail.com>
2016-09-26 20:33:28 +0000

    next (voting) function

--- test/core.spec.js
+++ test/core.spec.js
@@ -9,21 +9,35 @@ import {
 from 'chai';
 
 import {
-    setEntries
+    setEntries,
+    next
 }
 from '../src/core';
 
 describe('appication logic', () => {
-        describe('setEntries', () => {
-            it('add the entries to the state', () => {
-                const state = Map();
-                const entries = List.of('Trainspotting', '28 days later');
-                const nextState = setEntries(state, entries);
+    describe('setEntries', () => {
+        it('add the entries to the state', () => {
+            const state = Map();
+            const entries = List.of('Trainspotting', '28 days later');
+            const nextState = setEntries(state, entries);
 
-                expect(nextState).to.equal(Map({
-                    entries: List.of('Trainspotting', '28 days later')
-                }));
-            });
+            expect(nextState).to.equal(Map({
+                entries: List.of('Trainspotting', '28 days later')
+            }));
         });
-    }
-);
+    });
+    describe('next', () => {
+        it('takes next two entries under vote', () => {
+            const state = Map({
+                entries: List.of('Trainspotting', '28 Days Later', 'Sunshine')
+            });
+            const nextState = next(state);
+            expect(nextState).to.equal(Map({
+                vote: Map({
+                    pair: List.of('Trainspotting', '28 Days Later')
+                }),
+                entries: List.of('Sunshine')
+            }));
+        });
+    });
+});
