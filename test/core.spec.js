0b967e3cdafa40fa7f150c3cb9b6b591498a52ae
Maciej Sartys <sartys.maciej@gmail.com>
2016-09-27 10:42:24 +0000

    finding and processing vote winners

--- test/core.spec.js
+++ test/core.spec.js
@@ -1,88 +1,129 @@
 import {
-    List,
-    Map
+  List,
+  Map
 }
 from 'immutable';
+
 import {
-    expect
+  expect
 }
 from 'chai';
 
 import {
-    setEntries,
-    next,
-    vote
+  setEntries,
+  next,
+  vote
 }
 from '../src/core';
 
 describe('appication logic', () => {
-    describe('setEntries', () => {
-        it('add the entries to the state', () => {
-            const state = Map();
-            const entries = List.of('Trainspotting', '28 days later');
-            const nextState = setEntries(state, entries);
+  describe('setEntries', () => {
+    it('add the entries to the state', () => {
+      const state = Map();
+      const entries = List.of('Trainspotting', '28 days later');
+      const nextState = setEntries(state, entries);
 
-            expect(nextState).to.equal(Map({
-                entries: List.of('Trainspotting', '28 days later')
-            }));
-        });
+      expect(nextState).to.equal(Map({
+        entries: List.of('Trainspotting', '28 days later')
+      }));
     });
-    describe('next', () => {
-        it('takes next two entries under vote', () => {
-            const state = Map({
-                entries: List.of('Trainspotting', '28 Days Later', 'Sunshine')
-            });
-            const nextState = next(state);
-            expect(nextState).to.equal(Map({
-                vote: Map({
-                    pair: List.of('Trainspotting', '28 Days Later')
-                }),
-                entries: List.of('Sunshine')
-            }));
-        });
+  });
+  describe('next', () => {
+    it('takes next two entries under vote', () => {
+      const state = Map({
+        entries: List.of('Trainspotting', '28 Days Later', 'Sunshine')
+      });
+      const nextState = next(state);
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later')
+        }),
+        entries: List.of('Sunshine')
+      }));
     });
-    describe('vote', () => {
-        it('creates a tally for new vote entry', () => {
-            const state = Map({
-                vote: Map({
-                    pair: List.of('Trainspotting', '28 Days Later')
-                }),
-                entries: List()
-            });
-            const nextState = vote(state, 'Trainspotting');
-            expect(nextState).to.equal(Map({
-                vote: Map({
-                    pair: List.of('Trainspotting', '28 Days Later'),
-                    tally: Map({
-                        'Trainspotting': 1
-                    })
-                }),
-                entries: List()
-            }));
-        });
+    it('puts winner of a current vote at the back of the entires list', () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later'),
+          tally: Map({
+            'Trainspotting': 3,
+            '28 Days Later': 2
+          })
+        }),
+        entries: List.of('Sunshine', 'Millions', '127 Hours')
+      });
+      const nextState = next(state);
 
-        it('adds to existing tally for a voted entry', () => {
-            const state = Map({
-                vote: Map({
-                    pair: List.of('Trainspotting', '28 Days Later'),
-                    tally: Map({
-                        'Trainspotting': 3,
-                        '28 Days Later': 2
-                    })
-                }),
-                entries: List()
-            });
-            const nextState = vote(state, 'Trainspotting');
-            expect(nextState).to.equal(Map({
-                vote: Map({
-                    pair: List.of('Trainspotting', '28 Days Later'),
-                    tally: Map({
-                        'Trainspotting': 4,
-                        '28 Days Later': 2
-                    })
-                }),
-                entries: List()
-            }));
-        });
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Sunshine', 'Millions')
+        }),
+        entries: List.of('127 Hours', 'Trainspotting')
+      }));
     });
+    it('puts back both entries at the end of list if voting tied', () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later'),
+          tally: Map({
+            'Trainspotting': 2,
+            '28 Days Later': 2
+          })
+        }),
+        entries: List.of('Sunshine', 'Millions', '127 Hours')
+      });
+      const nextState = next(state);
+
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Sunshine', 'Millions')
+        }),
+        entries: List.of('127 Hours', 'Trainspotting', '28 Days Later')
+      }));
+    });
+  });
+  describe('vote', () => {
+    it('creates a tally for new vote entry', () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later')
+        }),
+        entries: List()
+      });
+      const nextState = vote(state, 'Trainspotting');
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later'),
+          tally: Map({
+            'Trainspotting': 1
+          })
+        }),
+        entries: List()
+      }));
+    });
+
+    it('adds to existing tally for a voted entry', () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later'),
+          tally: Map({
+            'Trainspotting': 3,
+            '28 Days Later': 2
+          })
+        }),
+        entries: List()
+      });
+      const nextState = vote(state, 'Trainspotting');
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later'),
+          tally: Map({
+            'Trainspotting': 4,
+            '28 Days Later': 2
+          })
+        }),
+        entries: List()
+      }));
+    });
+  });
 });
