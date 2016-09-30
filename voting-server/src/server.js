ad2c9b1e0bab6719bd576c05db13bf68cb657dda
Maciej Sartys <sartys.maciej@gmail.com>
2016-09-30 13:08:40 +0000

    socket.io emits application state

--- voting-server/src/server.js
+++ voting-server/src/server.js
@@ -1,7 +1,18 @@
 import Server from 'socket.io';
 
-export default function startServer() {
+export default function startServer(store) {
   const port = process.env.PORT;
   const io = new Server().attach(port);
+  
+  store.subscribe(
+    () => io.emit('state', store.getState().toJS())
+  );
+  
+  io.on('connection', (socket) => {
+    socket.emit('state', store.getState().toJS());
+    socket.on('action', store.dispach.bind(store));
+  });
+  
   console.log('Socket.io server running on port: ', port);
+  
 }
